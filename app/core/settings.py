10183763a2d4a4c4c406254e518210627fece2f4
Anton Cherepov <tauvau33@gmail.com>
2025-01-30 07:55:30 +0900

    Update settings.py

--- app/core/settings.py
+++ app/core/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'asdasdasdqewrbfiw9ufir9234929dhwo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]  # Для теста можно оставить "*"
+ALLOWED_HOSTS = ["*", '127.0.0.1']  # Для теста можно оставить "*"
 
 
 # Application definition
