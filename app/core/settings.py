5c8ebddfe02e542aeee28d1cac0c02ba4e08cad5
Anton Cherepov <tauvau33@gmail.com>
2025-01-30 08:18:01 +0900

    Update settings.py

--- app/core/settings.py
+++ app/core/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'mydb',
-        'USER': 'myuser',
-        'PASSWORD': 'mypass',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
